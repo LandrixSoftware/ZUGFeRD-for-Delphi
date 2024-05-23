9abbd5ccdcca13fe0e0e1c1e9cd3631892c7552d
Sven Harazim <sven.harazim@landrix.de>
2024-05-23 11:13:33 +0200

    prepare distribution tools

---
+++ Distribution/README.md
@@ -0,0 +1,25 @@
+# ZUGFeRD Tools
+
+Das Skript installtools.ps1 mit PowerShell ausfuehren, um die Tools zu installieren bzw. zu aktualisieren.
+
+## Mustang-CLI
+
+https://www.mustangproject.org/kommandozeile/?lang=de
+
+## Historie
+
+- 23.05.2024 Init\
+  https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u292-b10_openj9-0.26.0/OpenJDK8U-jre_x64_windows_openj9_8u292b10_openj9-0.26.0.zip
+  https://github.com/ZUGFeRD/mustangproject/releases/download/core-2.10.0/Mustang-CLI-2.10.0.jar
+  
+## Tool-Verzeichnis
+
+https://adoptopenjdk.net
+
+https://github.com/ZUGFeRD/mustangproject
+
+## Versionen
+
+https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u292-b10_openj9-0.26.0/OpenJDK8U-jre_x64_windows_openj9_8u292b10_openj9-0.26.0.zip
+
+https://github.com/ZUGFeRD/mustangproject/releases/download/core-2.10.0/Mustang-CLI-2.10.0.jar
---
+++ Distribution/installtools.ps1
@@ -0,0 +1,29 @@
+If (Test-Path java){
+  Remove-Item java -Recurse
+}
+
+If (Test-Path mustangproject){
+  Remove-Item mustangproject -Recurse
+}
+
+#If (Test-Path apachemaven){
+#  Remove-Item apachemaven -Recurse
+#}
+
+Invoke-WebRequest -Uri "https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.6%2B10/OpenJDK17U-jre_x64_windows_hotspot_17.0.6_10.zip" -OutFile jre.zip
+Invoke-WebRequest -Uri "https://github.com/ZUGFeRD/mustangproject/releases/download/core-2.10.0/Mustang-CLI-2.10.0.jar" -OutFile mustangproject\Mustang-CLI-2.10.0.jar
+#Invoke-WebRequest -Uri "https://dlcdn.apache.org/maven/maven-3/3.9.6/binaries/apache-maven-3.9.6-bin.zip" -OutFile apachemaven.zip
+
+https://www.mustangproject.org/deploy/Mustang-CLI-2.10.0.jar
+
+Expand-Archive jre.zip
+#Expand-Archive apachemaven.zip
+Move-Item .\jre\jdk-17.0.6+10-jre .\java
+Remove-Item jre -Recurse
+
+If (Test-Path jre.zip){
+  Remove-Item jre.zip
+}
+#If (Test-Path apachemaven.zip){
+#  Remove-Item apachemaven.zip
+#}
