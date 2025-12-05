d9e82df235dce242ac83e97ae92cd4281e955687
Sven Harazim <info@landrix.de>
2025-12-05 08:31:23 +0100

    Auto stash before merge of "main" and "origin/main"

--- Test/intf.ZUGFeRDTaxCategoryCodes.UnitTests.pas
+++ Test/intf.ZUGFeRDTaxCategoryCodes.UnitTests.pas
@@ -20,7 +20,7 @@ unit intf.ZUGFeRDTaxCategoryCodes.UnitTests;
 interface
 
 uses
-  DUnitX.TestFramework, intf.ZUGFeRDTaxCategoryCodes;
+  DUnitX.TestFramework, intf.ZUGFeRDTaxCategoryCodes, intf.ZUGFeRDHelper;
 
 type
   [TestFixture]
@@ -36,54 +36,54 @@ implementation
 
 procedure TZUGFeRDTaxCategoryCodesTest.TestFromString;
 begin
-  Assert.AreEqual(TZUGFeRDTaxCategoryCodes.A, TZUGFeRDTaxCategoryCodesExtensions.FromString('A').GetValueOrDefault);
-  Assert.AreEqual(TZUGFeRDTaxCategoryCodes.AA, TZUGFeRDTaxCategoryCodesExtensions.FromString('AA').GetValueOrDefault);
-  Assert.AreEqual(TZUGFeRDTaxCategoryCodes.AB, TZUGFeRDTaxCategoryCodesExtensions.FromString('AB').GetValueOrDefault);
-  Assert.AreEqual(TZUGFeRDTaxCategoryCodes.AC, TZUGFeRDTaxCategoryCodesExtensions.FromString('AC').GetValueOrDefault);
-  Assert.AreEqual(TZUGFeRDTaxCategoryCodes.AD, TZUGFeRDTaxCategoryCodesExtensions.FromString('AD').GetValueOrDefault);
-  Assert.AreEqual(TZUGFeRDTaxCategoryCodes.AE, TZUGFeRDTaxCategoryCodesExtensions.FromString('AE').GetValueOrDefault);
-  Assert.AreEqual(TZUGFeRDTaxCategoryCodes.B, TZUGFeRDTaxCategoryCodesExtensions.FromString('B').GetValueOrDefault);
-  Assert.AreEqual(TZUGFeRDTaxCategoryCodes.C, TZUGFeRDTaxCategoryCodesExtensions.FromString('C').GetValueOrDefault);
-  Assert.AreEqual(TZUGFeRDTaxCategoryCodes.D, TZUGFeRDTaxCategoryCodesExtensions.FromString('D').GetValueOrDefault);
-  Assert.AreEqual(TZUGFeRDTaxCategoryCodes.E, TZUGFeRDTaxCategoryCodesExtensions.FromString('E').GetValueOrDefault);
-  Assert.AreEqual(TZUGFeRDTaxCategoryCodes.F, TZUGFeRDTaxCategoryCodesExtensions.FromString('F').GetValueOrDefault);
-  Assert.AreEqual(TZUGFeRDTaxCategoryCodes.G, TZUGFeRDTaxCategoryCodesExtensions.FromString('G').GetValueOrDefault);
-  Assert.AreEqual(TZUGFeRDTaxCategoryCodes.H, TZUGFeRDTaxCategoryCodesExtensions.FromString('H').GetValueOrDefault);
-  Assert.AreEqual(TZUGFeRDTaxCategoryCodes.I, TZUGFeRDTaxCategoryCodesExtensions.FromString('I').GetValueOrDefault);
-  Assert.AreEqual(TZUGFeRDTaxCategoryCodes.J, TZUGFeRDTaxCategoryCodesExtensions.FromString('J').GetValueOrDefault);
-  Assert.AreEqual(TZUGFeRDTaxCategoryCodes.K, TZUGFeRDTaxCategoryCodesExtensions.FromString('K').GetValueOrDefault);
-  Assert.AreEqual(TZUGFeRDTaxCategoryCodes.L, TZUGFeRDTaxCategoryCodesExtensions.FromString('L').GetValueOrDefault);
-  Assert.AreEqual(TZUGFeRDTaxCategoryCodes.M, TZUGFeRDTaxCategoryCodesExtensions.FromString('M').GetValueOrDefault);
-  Assert.AreEqual(TZUGFeRDTaxCategoryCodes.O, TZUGFeRDTaxCategoryCodesExtensions.FromString('O').GetValueOrDefault);
-  Assert.AreEqual(TZUGFeRDTaxCategoryCodes.S, TZUGFeRDTaxCategoryCodesExtensions.FromString('S').GetValueOrDefault);
-  Assert.AreEqual(TZUGFeRDTaxCategoryCodes.Z, TZUGFeRDTaxCategoryCodesExtensions.FromString('Z').GetValueOrDefault);
-  Assert.AreEqual(TZUGFeRDTaxCategoryCodes.Unknown, TZUGFeRDTaxCategoryCodesExtensions.FromString('Invalid').GetValueOrDefault);
+  Assert.AreEqual(TZUGFeRDTaxCategoryCodes.A, TEnumExtensions<TZUGFeRDTaxCategoryCodes>.StringToEnum('A'));
+  Assert.AreEqual(TZUGFeRDTaxCategoryCodes.AA, TEnumExtensions<TZUGFeRDTaxCategoryCodes>.StringToEnum('AA'));
+  Assert.AreEqual(TZUGFeRDTaxCategoryCodes.AB, TEnumExtensions<TZUGFeRDTaxCategoryCodes>.StringToEnum('AB'));
+  Assert.AreEqual(TZUGFeRDTaxCategoryCodes.AC, TEnumExtensions<TZUGFeRDTaxCategoryCodes>.StringToEnum('AC'));
+  Assert.AreEqual(TZUGFeRDTaxCategoryCodes.AD, TEnumExtensions<TZUGFeRDTaxCategoryCodes>.StringToEnum('AD'));
+  Assert.AreEqual(TZUGFeRDTaxCategoryCodes.AE, TEnumExtensions<TZUGFeRDTaxCategoryCodes>.StringToEnum('AE'));
+  Assert.AreEqual(TZUGFeRDTaxCategoryCodes.B, TEnumExtensions<TZUGFeRDTaxCategoryCodes>.StringToEnum('B'));
+  Assert.AreEqual(TZUGFeRDTaxCategoryCodes.C, TEnumExtensions<TZUGFeRDTaxCategoryCodes>.StringToEnum('C'));
+  Assert.AreEqual(TZUGFeRDTaxCategoryCodes.D, TEnumExtensions<TZUGFeRDTaxCategoryCodes>.StringToEnum('D'));
+  Assert.AreEqual(TZUGFeRDTaxCategoryCodes.E, TEnumExtensions<TZUGFeRDTaxCategoryCodes>.StringToEnum('E'));
+  Assert.AreEqual(TZUGFeRDTaxCategoryCodes.F, TEnumExtensions<TZUGFeRDTaxCategoryCodes>.StringToEnum('F'));
+  Assert.AreEqual(TZUGFeRDTaxCategoryCodes.G, TEnumExtensions<TZUGFeRDTaxCategoryCodes>.StringToEnum('G'));
+  Assert.AreEqual(TZUGFeRDTaxCategoryCodes.H, TEnumExtensions<TZUGFeRDTaxCategoryCodes>.StringToEnum('H'));
+  Assert.AreEqual(TZUGFeRDTaxCategoryCodes.I, TEnumExtensions<TZUGFeRDTaxCategoryCodes>.StringToEnum('I'));
+  Assert.AreEqual(TZUGFeRDTaxCategoryCodes.J, TEnumExtensions<TZUGFeRDTaxCategoryCodes>.StringToEnum('J'));
+  Assert.AreEqual(TZUGFeRDTaxCategoryCodes.K, TEnumExtensions<TZUGFeRDTaxCategoryCodes>.StringToEnum('K'));
+  Assert.AreEqual(TZUGFeRDTaxCategoryCodes.L, TEnumExtensions<TZUGFeRDTaxCategoryCodes>.StringToEnum('L'));
+  Assert.AreEqual(TZUGFeRDTaxCategoryCodes.M, TEnumExtensions<TZUGFeRDTaxCategoryCodes>.StringToEnum('M'));
+  Assert.AreEqual(TZUGFeRDTaxCategoryCodes.O, TEnumExtensions<TZUGFeRDTaxCategoryCodes>.StringToEnum('O'));
+  Assert.AreEqual(TZUGFeRDTaxCategoryCodes.S, TEnumExtensions<TZUGFeRDTaxCategoryCodes>.StringToEnum('S'));
+  Assert.AreEqual(TZUGFeRDTaxCategoryCodes.Z, TEnumExtensions<TZUGFeRDTaxCategoryCodes>.StringToEnum('Z'));
+  Assert.AreEqual(TZUGFeRDTaxCategoryCodes.Unknown, TEnumExtensions<TZUGFeRDTaxCategoryCodes>.StringToEnum('Invalid'));
 end;
 
 procedure TZUGFeRDTaxCategoryCodesTest.TestEnumToString;
 begin
-  Assert.AreEqual('A', TZUGFeRDTaxCategoryCodesExtensions.EnumToString(TZUGFeRDTaxCategoryCodes.A));
-  Assert.AreEqual('AA', TZUGFeRDTaxCategoryCodesExtensions.EnumToString(TZUGFeRDTaxCategoryCodes.AA));
-  Assert.AreEqual('AB', TZUGFeRDTaxCategoryCodesExtensions.EnumToString(TZUGFeRDTaxCategoryCodes.AB));
-  Assert.AreEqual('AC', TZUGFeRDTaxCategoryCodesExtensions.EnumToString(TZUGFeRDTaxCategoryCodes.AC));
-  Assert.AreEqual('AD', TZUGFeRDTaxCategoryCodesExtensions.EnumToString(TZUGFeRDTaxCategoryCodes.AD));
-  Assert.AreEqual('AE', TZUGFeRDTaxCategoryCodesExtensions.EnumToString(TZUGFeRDTaxCategoryCodes.AE));
-  Assert.AreEqual('B', TZUGFeRDTaxCategoryCodesExtensions.EnumToString(TZUGFeRDTaxCategoryCodes.B));
-  Assert.AreEqual('C', TZUGFeRDTaxCategoryCodesExtensions.EnumToString(TZUGFeRDTaxCategoryCodes.C));
-  Assert.AreEqual('D', TZUGFeRDTaxCategoryCodesExtensions.EnumToString(TZUGFeRDTaxCategoryCodes.D));
-  Assert.AreEqual('E', TZUGFeRDTaxCategoryCodesExtensions.EnumToString(TZUGFeRDTaxCategoryCodes.E));
-  Assert.AreEqual('F', TZUGFeRDTaxCategoryCodesExtensions.EnumToString(TZUGFeRDTaxCategoryCodes.F));
-  Assert.AreEqual('G', TZUGFeRDTaxCategoryCodesExtensions.EnumToString(TZUGFeRDTaxCategoryCodes.G));
-  Assert.AreEqual('H', TZUGFeRDTaxCategoryCodesExtensions.EnumToString(TZUGFeRDTaxCategoryCodes.H));
-  Assert.AreEqual('I', TZUGFeRDTaxCategoryCodesExtensions.EnumToString(TZUGFeRDTaxCategoryCodes.I));
-  Assert.AreEqual('J', TZUGFeRDTaxCategoryCodesExtensions.EnumToString(TZUGFeRDTaxCategoryCodes.J));
-  Assert.AreEqual('K', TZUGFeRDTaxCategoryCodesExtensions.EnumToString(TZUGFeRDTaxCategoryCodes.K));
-  Assert.AreEqual('L', TZUGFeRDTaxCategoryCodesExtensions.EnumToString(TZUGFeRDTaxCategoryCodes.L));
-  Assert.AreEqual('M', TZUGFeRDTaxCategoryCodesExtensions.EnumToString(TZUGFeRDTaxCategoryCodes.M));
-  Assert.AreEqual('O', TZUGFeRDTaxCategoryCodesExtensions.EnumToString(TZUGFeRDTaxCategoryCodes.O));
-  Assert.AreEqual('S', TZUGFeRDTaxCategoryCodesExtensions.EnumToString(TZUGFeRDTaxCategoryCodes.S));
-  Assert.AreEqual('Z', TZUGFeRDTaxCategoryCodesExtensions.EnumToString(TZUGFeRDTaxCategoryCodes.Z));
-  Assert.AreEqual('Unknown', TZUGFeRDTaxCategoryCodesExtensions.EnumToString(TZUGFeRDTaxCategoryCodes.Unknown));
+  Assert.AreEqual('A', TEnumExtensions<TZUGFeRDTaxCategoryCodes>.EnumToString(TZUGFeRDTaxCategoryCodes.A));
+  Assert.AreEqual('AA', TEnumExtensions<TZUGFeRDTaxCategoryCodes>.EnumToString(TZUGFeRDTaxCategoryCodes.AA));
+  Assert.AreEqual('AB', TEnumExtensions<TZUGFeRDTaxCategoryCodes>.EnumToString(TZUGFeRDTaxCategoryCodes.AB));
+  Assert.AreEqual('AC', TEnumExtensions<TZUGFeRDTaxCategoryCodes>.EnumToString(TZUGFeRDTaxCategoryCodes.AC));
+  Assert.AreEqual('AD', TEnumExtensions<TZUGFeRDTaxCategoryCodes>.EnumToString(TZUGFeRDTaxCategoryCodes.AD));
+  Assert.AreEqual('AE', TEnumExtensions<TZUGFeRDTaxCategoryCodes>.EnumToString(TZUGFeRDTaxCategoryCodes.AE));
+  Assert.AreEqual('B', TEnumExtensions<TZUGFeRDTaxCategoryCodes>.EnumToString(TZUGFeRDTaxCategoryCodes.B));
+  Assert.AreEqual('C', TEnumExtensions<TZUGFeRDTaxCategoryCodes>.EnumToString(TZUGFeRDTaxCategoryCodes.C));
+  Assert.AreEqual('D', TEnumExtensions<TZUGFeRDTaxCategoryCodes>.EnumToString(TZUGFeRDTaxCategoryCodes.D));
+  Assert.AreEqual('E', TEnumExtensions<TZUGFeRDTaxCategoryCodes>.EnumToString(TZUGFeRDTaxCategoryCodes.E));
+  Assert.AreEqual('F', TEnumExtensions<TZUGFeRDTaxCategoryCodes>.EnumToString(TZUGFeRDTaxCategoryCodes.F));
+  Assert.AreEqual('G', TEnumExtensions<TZUGFeRDTaxCategoryCodes>.EnumToString(TZUGFeRDTaxCategoryCodes.G));
+  Assert.AreEqual('H', TEnumExtensions<TZUGFeRDTaxCategoryCodes>.EnumToString(TZUGFeRDTaxCategoryCodes.H));
+  Assert.AreEqual('I', TEnumExtensions<TZUGFeRDTaxCategoryCodes>.EnumToString(TZUGFeRDTaxCategoryCodes.I));
+  Assert.AreEqual('J', TEnumExtensions<TZUGFeRDTaxCategoryCodes>.EnumToString(TZUGFeRDTaxCategoryCodes.J));
+  Assert.AreEqual('K', TEnumExtensions<TZUGFeRDTaxCategoryCodes>.EnumToString(TZUGFeRDTaxCategoryCodes.K));
+  Assert.AreEqual('L', TEnumExtensions<TZUGFeRDTaxCategoryCodes>.EnumToString(TZUGFeRDTaxCategoryCodes.L));
+  Assert.AreEqual('M', TEnumExtensions<TZUGFeRDTaxCategoryCodes>.EnumToString(TZUGFeRDTaxCategoryCodes.M));
+  Assert.AreEqual('O', TEnumExtensions<TZUGFeRDTaxCategoryCodes>.EnumToString(TZUGFeRDTaxCategoryCodes.O));
+  Assert.AreEqual('S', TEnumExtensions<TZUGFeRDTaxCategoryCodes>.EnumToString(TZUGFeRDTaxCategoryCodes.S));
+  Assert.AreEqual('Z', TEnumExtensions<TZUGFeRDTaxCategoryCodes>.EnumToString(TZUGFeRDTaxCategoryCodes.Z));
+  Assert.AreEqual('Unknown', TEnumExtensions<TZUGFeRDTaxCategoryCodes>.EnumToString(TZUGFeRDTaxCategoryCodes.Unknown));
 end;
 
 initialization
